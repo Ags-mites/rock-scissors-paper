2eca03351beddd1dcf7421addf12211de5b4f420
Agustin Mites <mites96@hotmail.com>
2020-10-01 22:37:15 -0500

    juego de p,r,t

---
+++ src/js/juego.js
@@ -0,0 +1,22 @@
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+const rock = document.getElementById('rock')
+
+console.log(rock)
+console.log(scissors)
+console.log(paper)
+
+
+const  optionpc = () => Math.floor(Math.random() * (3 - 0))
+
+const game= () =>{
+    
+}
+
+
+let num1 = 19;
+let num2 = 341;
+
+const result = (num1, num2) => (num1*100)/num2
+
+console.log(result(num1, num2))
