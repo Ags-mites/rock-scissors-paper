68fb5d139062a476b9f634571055cbc89a9272c0
Agustin Mites <mites96@hotmail.com>
2021-01-02 18:44:40 -0500

    aun faltan corregir errores

--- src/js/juego.js
+++ src/js/juego.js
@@ -6,11 +6,13 @@ const result = document.getElementById("result")
 const textResult = document.getElementById("text__result")
 const YouPicked = document.getElementById("YouPicked")
 const TheHousePicked = document.getElementById("TheHousePicked")
+const score = document.getElementById("score")
 let classButtons = [
     "button__blue",
     "button__yellow",
     "button__red"
 ]
+let historyScore = 0;
 
 paper.onclick = () => Game(0)
 scissors.onclick = () => Game(1)
@@ -19,7 +21,6 @@ rock.onclick = () => Game(2)
 function Game(player) {
     const selectionPC =  Math.floor(Math.random() * 3)
     const traslatePlayer = traslateNumbertoLetter(player)
-    const traslatePC = traslateNumbertoLetter(selectionPC)
     buttonsClass(player, selectionPC)
     if (player == selectionPC){
         textResult.innerHTML = `EMPATE escojieron ${traslatePlayer}`
@@ -29,15 +30,16 @@ function Game(player) {
             //papel
             if(selectionPC == 2){
                 textResult.innerHTML = `YOU WIN`
+                historyScore = +10
             }
             else if(selectionPC == 1){
-                
                 textResult.innerHTML = `YOU LOSE`
             }
         }else if(player === 1 ){
             //tijeras
             if(selectionPC == 0){
                 textResult.innerHTML = `YOU WIN`
+                historyScore = +10
             }
             else if(selectionPC == 2){
                 textResult.innerHTML = `YOU LOSE`
@@ -46,12 +48,13 @@ function Game(player) {
             //piedra 
             if(selectionPC == 1){
                 textResult.innerHTML = `YOU WIN`
+                historyScore = +10
             }
             else if(selectionPC == 0){
                 textResult.innerHTML = `YOU LOSE`
             }
         }
-        
+        refreschScore(historyScore)
     }
 }
 
@@ -71,4 +74,15 @@ function  traslateNumbertoLetter(player){
         case 2:
             return "roca"
     }
+}
+
+function playAgain(){
+    ingame.classList.remove("hide");
+    result.classList.add("hide");
+    historyScore = 0
+    refreschScore(historyScore)
+}
+
+function refreschScore(historyScore){
+    score.innerHTML = historyScore
 }
